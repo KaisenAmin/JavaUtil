7cd8222a14ecfe7f708ff16a18a076fb8e1006ea
amin tahmasebi <amintahmasebi479@gmail.com>
2023-07-09 11:25:25 +0330

    create Vector class in java_util also create and implement 3 constructor and add, get methods with an example in examples/java_util/Vector_1.cpp

--- include/java_util/Vector.h
+++ include/java_util/Vector.h
@@ -1,8 +1,31 @@
 //
-// Created by Kaisen on 7/5/2023.
+// Created by Amin Tahmasebi on 7/5/2023.
 //
 
 #ifndef JAVAUTIL_VECTOR_H
 #define JAVAUTIL_VECTOR_H
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+template <typename T>
+class Vector
+{
+private:
+    std::vector<T> elementData;
+    int elementCount = 0;
+    int capacityIncrement;
+
+public:
+    Vector();
+    Vector(int initialCapacity);
+    Vector(int initialCapacity, int capacityIncrement);
+    void add(const T& value);
+    T get(int index) const;
+};
+
+#include "../../src/java_util/Vector.cpp"
 
 #endif //JAVAUTIL_VECTOR_H
--- src/java_util/Vector.cpp
+++ src/java_util/Vector.cpp
@@ -1,3 +1,44 @@
 //
-// Created by Kaisen on 7/5/2023.
+// Created by Amin Tahmasebi on 7/5/2023.
 //
+
+#include "../../include/java_util/Vector.h"
+
+template <typename T>
+Vector<T>::Vector() : capacityIncrement(0) {}
+
+template <typename T>
+Vector<T>::Vector(int initialCapacity) : capacityIncrement(0)
+{
+    elementData.reserve(initialCapacity);
+}
+
+template <typename T>
+Vector<T>::Vector(int initialCapacity, int capacityIncrement)
+        : capacityIncrement(capacityIncrement)
+        {
+    elementData.reserve(initialCapacity);
+}
+
+template <typename T>
+void Vector<T>::add(const T& value)
+{
+    if(elementCount >= elementData.capacity())
+    {
+        elementData.reserve(elementData.capacity() + (capacityIncrement > 0 ? capacityIncrement : 1));
+    }
+    elementData.push_back(value);
+    ++elementCount;
+}
+
+template <typename T>
+T Vector<T>::get(int index) const
+{
+    if(index >= elementCount)
+    {
+        throw std::out_of_range("Index out of range");
+    }
+
+    return elementData[index];
+}
+
